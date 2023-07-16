2b01a5c830085d70c46187590f1cfd94407902b2
abdiaelema76 <gababgoabdiaelema@gmail.com>
2023-07-16 23:21:26 +0300

    changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """initializes storage variable to create FileSorage object for hbnb application"""
+
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
