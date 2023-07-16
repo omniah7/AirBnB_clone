31ad508f5fe113a55236fc7786c6adf6fc174aaa
omniah7 <om267165@gmail.com>
2023-07-17 00:25:48 +0300

    base_model_test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,83 +10,83 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-def all(self):
-    """returns the dictionary __objects"""
-    return FileStorage.__objects
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
 
-def new(self, obj):
-    """ sets in __objects the obj with key <obj class name>.id"""
-    key = "{}.{}".format(type(obj).__name__,obj.id)
-    FileStorage.__objects[key] = obj
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__,obj.id)
+        FileStorage.__objects[key] = obj
 
-def save(self):
-    """serializes __objects to the JSON file (path: __file_path)"""
-    with open(FileStorage.__file_path, mode="w", encoding="utf-8")as my_file:
-        d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-        json.dump(d, my_file)
-def classes(self):
-    """Returns dictionary of various classes"""
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.state import State
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8")as my_file:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, my_file)
+    def classes(self):
+        """Returns dictionary of various classes"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
 
-    classes = {"BaseModel": BaseModel,
-               "User": User,
-               "City": City,
-               "Amenity": Amenity,
-               "Place": Place,
-               "Review": Review}
-    return classes
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
-def reload(self):
-    """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
-    otherwise, do nothing. If the file doesn’t exist, no exception should be raised
-    """
-    if not os.path.isfile(FileStorage.__file_path):
-        return
-    with open(FileStorage.__file_path, mode="r", encoding="utf-8")as my_file:
-        obj_dict = json.load(my_file)
-        obj_dict = {k: self.classes()[v["__class__"]](**v)
-            for k, v in obj_dict.items()}
-        FileStorage.__objects = obj_dict
-def attributes(self):
-    attributes = {
-        "BaseModel":{
-            "id": str,
-            "created_at": datetime.datetime,
-            "updated_at": datetime.datetime},
-        "User":{
-            "email": str,
-            "password": str,
-            "first_name": str,
-            "last_name": str},
-       "State":{
-            "name": str},
-       "City":{
-            "state_id": str,
-            "name": str},
-       "Amenity":{
-            "name": str},
-       "Place":{
-            "city_id": str,
-            "user_id": str,
-            "name": str,
-            "description": str,
-            "number_rooms": int,
-            "number_bathrooms": int,
-            "max_guest": int,
-            "price_by_night": int,
-            "latitude": float,
-            "longitude": float,
-            "amenity_ids": list},
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised
+        """
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, mode="r", encoding="utf-8")as my_file:
+            obj_dict = json.load(my_file)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+    def attributes(self):
+        attributes = {
+            "BaseModel":{
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime},
+            "User":{
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str},
+           "State":{
+                "name": str},
+           "City":{
+                "state_id": str,
+                "name": str},
+           "Amenity":{
+                "name": str},
+           "Place":{
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list},
 
-        "Review":{
-            "place_id": str,
-            "user_id": str,
-            "text": str}
-    }
-    return attributes
+            "Review":{
+                "place_id": str,
+                "user_id": str,
+                "text": str}
+        }
+        return attributes
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,47 @@
 """Test BaseModel"""
 
 import unittest
-from models.base_model import BaseModels
+from models.base_model import BaseModel
 
 class test_BaseModel(unittest.TestCase):
     """Test BaseModel Class"""
-    def test_attr(self):
+    def setUp(self):
+        """create two test instances"""
+        pass
+
+    def TearDown(self):
+        """ delete json file """
+        pass
+
+    def test_attributes(self):
         """Tests for attriutes"""
+        obj1 = BaseModel()
+        self.assertTrue(hasattr(obj1, "id"))
+        self.assertTrue(hasattr(obj1, "created_at"))
+        self.assertTrue(hasattr(obj1, "updated_at"))
+        obj2 = BaseModel()
+        self.assertTrue(hasattr(obj2, "id"))
+        self.assertTrue(hasattr(obj2, "created_at"))
+        self.assertTrue(hasattr(obj2, "updated_at"))
+        self.assertNotEqual(obj1.id, obj2.id)
+        self.assertNotEqual(obj1.created_at, obj2.created_at)
+        self.assertNotEqual(obj1.updated_at, obj2.updated_at)
+        
+
+    def test_str(self):
+        """test the string format"""
+        obj3 = BaseModel()
+        self.assertTrue(hasattr(obj3, "id"))
+        self.assertEqual(str(obj3), f"[BaseModel] ({obj3.id}) {obj3.__dict__}")
+
+    def test_to_dict(self):
+        """test the dict format"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['id'], obj.id)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+        self.assertEqual(type(obj_dict['created_at']), str)
+        self.assertEqual(type(obj_dict['created_at']), str)
+
